ac4fe74647737ef4762777dee445ed12408981d6
Michael <liukaiyi54@gmail.com>
2015-09-01 19:06:15 +0800

    Initial Commit

---
+++ DynamicsPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicsPlayground
+//
+//  Created by Michael on 9/1/15.
+//  Copyright (c) 2015 Michael's None-Exist Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicsPlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicsPlayground
+//
+//  Created by Michael on 9/1/15.
+//  Copyright (c) 2015 Michael's None-Exist Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
